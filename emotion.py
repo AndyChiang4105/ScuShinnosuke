7459a9132fc4975932438787fe598c208cfba8e4
Zihao1028 <147487437+Zihao1028@users.noreply.github.com>
2024-06-18 21:45:28 +0800

    Update emotion.py

--- emotion.py
+++ emotion.py
@@ -4,7 +4,7 @@ from pprint import pprint
 
 def main():
         
-    url = "https://aip.baidubce.com/rpc/2.0/nlp/v1/sentiment_classify?access_token=24.04851c1c215f7605de1502f9f95ca113.2592000.1720877890.282335-82084981"
+    url = "https://aip.baidubce.com/rpc/2.0/nlp/v1/sentiment_classify?access_token="
     
     payload = json.dumps({
         "text":"我今天太伤心了，因为我摔了一跤，呜呜呜 " #input text用json存
